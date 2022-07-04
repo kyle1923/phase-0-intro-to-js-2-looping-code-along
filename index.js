f097efbfce09d9ee90c1e1075c37d63dd19c9cf6
Kyle Vermillion <kvermillion23@gmail.com>
2022-07-04 17:33:23 -0400

    commit tree cab8c358206b0a925b4ba9eb073a3f785f4438db

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(cards, event) {
+    let messages = []
+    for (let i = 0; i < cards.length; i++) {
+      messages.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+    }
+
+
+console.log(writeCards(names));
+function countDown() {
+let countDown = 10;
+while (countDown >= 0) {
+    console.log(countDown--);
+}
+}
